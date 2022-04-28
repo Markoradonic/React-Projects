9b375345ca75c6cd9091f70a12e55180a7e20962
Marko Radonic <marko.radonic@inform-technologies.com>
2022-04-28 17:32:26 +0200

    added meals lists

---
+++ 01-starting-project/src/components/Meals/Meals.js
@@ -0,0 +1,17 @@
+/* this will be responsible for that meals list... */
+
+import { Fragment } from 'react';
+
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
